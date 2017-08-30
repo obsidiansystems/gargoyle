680ef5621e764ec7333fcc6713db62cc98401489
Dan Haraj <dan@obsidian.systems>
2017-08-30 11:15:51 -0400

    Clean up .cabal and .nix files

--- gargoyle-postgresql/shell.nix
+++ gargoyle-postgresql/shell.nix
@@ -4,33 +4,11 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, bytestring, directory, gargoyle
-      , process, stdenv, stringsearch, text, unix
-      }:
-      mkDerivation {
-        pname = "gargoyle-postgresql";
-        version = "0.1";
-        src = ./.;
-        isLibrary = true;
-        isExecutable = true;
-        libraryHaskellDepends = [
-          base bytestring directory gargoyle process stringsearch text unix
-        ];
-        executableHaskellDepends = [
-          base bytestring gargoyle process text unix
-        ];
-        testHaskellDepends = [
-          pkgs.postgresql
-        ];
-        description = "Manage PostgreSQL servers with gargoyle";
-        license = stdenv.lib.licenses.bsd3;
-      };
-
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+  drv = haskellPackages.callPackage ./. {};
 
 in
 
--- gargoyle/shell.nix
+++ gargoyle/shell.nix
@@ -4,25 +4,11 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, directory, filepath, network, process
-      , stdenv, unix
-      }:
-      mkDerivation {
-        pname = "gargoyle";
-        version = "0.1";
-        src = ./.;
-        libraryHaskellDepends = [
-          base directory filepath network process unix
-        ];
-        description = "Automatically spin up and spin down local daemons";
-        license = stdenv.lib.licenses.bsd3;
-      };
-
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage f {};
+  drv = haskellPackages.callPackage ./. {};
 
 in
 
