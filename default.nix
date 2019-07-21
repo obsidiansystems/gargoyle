cc8f6ad46c57842833e1ec4e2c8fed30fc1576f1
Ali Abrar <aliabrar@gmail.com>
2019-07-21 01:23:30 -0400

    Add gargoyle-postgresql-connect

--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@ haskellPackages: rec {
   gargoyle = haskellPackages.callPackage ./gargoyle {};
   gargoyle-postgresql = haskellPackages.callPackage ./gargoyle-postgresql { inherit gargoyle; };
   gargoyle-postgresql-nix = haskellPackages.callPackage ./gargoyle-postgresql-nix { inherit gargoyle gargoyle-postgresql; };
+  gargoyle-postgresql-connect = haskellPackages.callPackage ./gargoyle-postgresql-connect { inherit gargoyle gargoyle-postgresql gargoyle-postgresql-nix; };
 }
