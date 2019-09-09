e368291536a5e252d62d43c62b35a266a60b7a75
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-09-09 15:23:11 +0100

    Use upstream 'staticWhich'

--- default.nix
+++ default.nix
@@ -1,4 +1,10 @@
 haskellPackages: rec {
+  which = haskellPackages.callHackageDirect {
+    pkg = "which";
+    ver = "0.1.0.0";
+    sha256 = "1c8svdiv378ps63lwn3aw7rv5wamlpmzgcn21r2pap4sx7p08892";
+  } {};
+
   gargoyle = haskellPackages.callCabal2nix "gargoyle" ./gargoyle {};
   gargoyle-postgresql = haskellPackages.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql { };
   gargoyle-postgresql-nix = haskellPackages.callPackage ./gargoyle-postgresql-nix { };
