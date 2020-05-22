8241adae18af0914adaca6f0f92b98ef4b17eaae
Leon P Smith <leon.smith@obsidian.systems>
2020-05-22 14:00:28 -0400

    Allow postgresql package to be specified

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-haskellPackages: {
+{ haskellPackages, postgresql ? null }: {
   which = haskellPackages.callHackageDirect {
     pkg = "which";
     ver = "0.1.0.0";
@@ -7,9 +7,9 @@ haskellPackages: {
 
   gargoyle = haskellPackages.callCabal2nix "gargoyle" ./gargoyle {};
   gargoyle-postgresql = haskellPackages.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql { };
-  gargoyle-postgresql-nix = haskellPackages.callPackage ({ pkgs, postgresql }:
+  gargoyle-postgresql-nix = haskellPackages.callPackage ({ pkgs }:
     pkgs.haskell.lib.overrideCabal (haskellPackages.callCabal2nix "gargoyle-postgresql-nix" ./gargoyle-postgresql-nix { }) (drv: {
-      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ postgresql ];
+      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (if postgresql == null then pkgs.postgresql else postgresql) ];
     })) {};
   gargoyle-postgresql-connect = haskellPackages.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect { };
 }
