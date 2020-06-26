3056377736083114b8ade1f1cecdede77d3216f3
Elliot Cameron <eacameron@gmail.com>
2020-06-26 12:00:43 -0400

    Fix nix infinite loop

--- release.nix
+++ release.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import ./dep/nixpkgs {} }:
 let haskellPackages = nixpkgs.haskellPackages.override {
-      overrides = self: super: import ./. self;
+      overrides = self: super: import ./. { haskellPackages = self; };
     };
 in {
   inherit (haskellPackages)
