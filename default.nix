ff476b83ed55ae5ded272adb19c8589d91854cfa
Elliot Cameron <eacameron@gmail.com>
2020-06-26 12:06:39 -0400

    Simplify nix

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ haskellPackages, postgresql ? null }: {
+{ haskellPackages
+, pkgs ? haskellPackages.callPackage ({pkgs}: pkgs) {}
+, postgresql ? pkgs.postgresql
+}: {
   gargoyle = haskellPackages.callCabal2nix "gargoyle" ./gargoyle {};
-  gargoyle-postgresql = haskellPackages.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql { };
-  gargoyle-postgresql-nix = haskellPackages.callPackage ({ pkgs }:
-    pkgs.haskell.lib.overrideCabal (haskellPackages.callCabal2nix "gargoyle-postgresql-nix" ./gargoyle-postgresql-nix { }) (drv: {
+  gargoyle-postgresql = haskellPackages.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql {};
+  gargoyle-postgresql-nix = pkgs.haskell.lib.overrideCabal
+    (haskellPackages.callCabal2nix "gargoyle-postgresql-nix" ./gargoyle-postgresql-nix {})
+    (drv: {
       librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (if postgresql == null then pkgs.postgresql else postgresql) ];
-    })) {};
-  gargoyle-postgresql-connect = haskellPackages.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect { };
+    });
+  gargoyle-postgresql-connect = haskellPackages.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect {};
 }
