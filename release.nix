aca5ee9eefe713942399e2abf1b488af24a9685c
Elliot Cameron <eacameron@gmail.com>
2020-06-26 12:02:36 -0400

    Move which override to release.nix so it doesn't get applied to users

--- release.nix
+++ release.nix
@@ -1,7 +1,14 @@
 { nixpkgs ? import ./dep/nixpkgs {} }:
-let haskellPackages = nixpkgs.haskellPackages.override {
-      overrides = self: super: import ./. { haskellPackages = self; };
+let
+  haskellPackages = nixpkgs.haskellPackages.override {
+    overrides = self: super: import ./. { haskellPackages = self; } // {
+      which = haskellPackages.callHackageDirect {
+        pkg = "which";
+        ver = "0.1.0.0";
+        sha256 = "1c8svdiv378ps63lwn3aw7rv5wamlpmzgcn21r2pap4sx7p08892";
+      } {};
     };
+  };
 in {
   inherit (haskellPackages)
     gargoyle
