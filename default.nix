e20be7bc659ba6b4dedf960e97cdf3a93a9ad7fa
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-10-19 19:16:24 -0400

    Apply ryan trinkle's last set of comments (described below)
    1. Get rid of pkgs argument in default.nix.
    2. Get rid of null check for postgresql argument in default.nix.
    3. Use the with keyword to make shorter function calls in default.nix.
    4. Only put gargoyle-postgresql-nix as a member of the
    librarySystemDepends list for gargoyle-postgresql-nix-monitor.
    5. Take out commented code in any cabal file.
    6. Use composeExtensions.

--- default.nix
+++ default.nix
@@ -1,22 +1,18 @@
-{pkgs ? null, postgresql ? null}: self: super:
-  let pkgs' = if pkgs == null then self.callPackage ({pkgs}: pkgs) {} else pkgs;
-      postgresql' = if postgresql == null then pkgs'.postgresql else postgresql;
-  in {
+self: super:
+   let pkgs = self.callPackage ({pkgs}:pkgs) {};
+   in with pkgs.haskell.lib; {postgresql ? pkgs.postgresql}: {
   gargoyle = self.callCabal2nix "gargoyle" ./gargoyle {};
   gargoyle-postgresql = self.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql {};
-  gargoyle-postgresql-nix = pkgs'.haskell.lib.overrideCabal
+  gargoyle-postgresql-nix = overrideCabal
     (self.callCabal2nix "gargoyle-postgresql-nix" ./gargoyle-postgresql-nix {})
     (drv: {
-      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (if postgresql' == null then pkgs'.postgresql else postgresql') ];
+      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ postgresql ];
     });
-  gargoyle-postgresql-nix-monitor = pkgs'.haskell.lib.justStaticExecutables (pkgs'.haskell.lib.overrideCabal
+  gargoyle-postgresql-nix-monitor = justStaticExecutables (overrideCabal
     (self.callCabal2nix "gargoyle-postgresql-nix-monitor" ./gargoyle-postgresql-nix-monitor {})
     (drv:{
-      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (pkgs'.haskell.lib.justStaticExecutables self.gargoyle-postgresql-nix) ];
+      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ self.gargoyle-postgresql-nix ];
     }));
-  gargoyle-postgresql-connect = pkgs'.haskell.lib.justStaticExecutables (pkgs'.haskell.lib.overrideCabal
-    (self.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect {})
-    (drv: {
-      librarySystemDepends = (drv.librarySystemDepends or []) ++ [(pkgs'.haskell.lib.justStaticExecutables self.gargoyle-postgresql-nix)];
-    }));
+  gargoyle-postgresql-connect =
+    justStaticExecutables (self.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect {});
 }
