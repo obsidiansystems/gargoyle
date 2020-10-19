6bb2a0721a79d350253f37062aa9692b58764752
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-10-19 16:03:50 -0400

    changed default.nix to overlay style

--- default.nix
+++ default.nix
@@ -1,18 +1,18 @@
-{ haskellPackages
-, pkgs ? haskellPackages.callPackage ({pkgs}: pkgs) {}
-, postgresql ? pkgs.postgresql
-}: rec {
-  gargoyle = haskellPackages.callCabal2nix "gargoyle" ./gargoyle {};
-  gargoyle-postgresql = haskellPackages.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql {};
+self: super:
+  let pkgs = self.callPackage ({pkgs}: pkgs) {};
+      postgresql = pkgs.postgresql;
+  in {
+  gargoyle = self.callCabal2nix "gargoyle" ./gargoyle {};
+  gargoyle-postgresql = self.callCabal2nix "gargoyle-postgresql" ./gargoyle-postgresql {};
   gargoyle-postgresql-nix = pkgs.haskell.lib.overrideCabal
-    (haskellPackages.callCabal2nix "gargoyle-postgresql-nix" ./gargoyle-postgresql-nix {})
+    (self.callCabal2nix "gargoyle-postgresql-nix" ./gargoyle-postgresql-nix {})
     (drv: {
       librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (if postgresql == null then pkgs.postgresql else postgresql) ];
     });
-  gargoyle-postgresql-nix-monitor = pkgs.haskell.lib.overrideCabal
-    (haskellPackages.callCabal2nix "gargoyle-postgresql-nix-monitor" ./gargoyle-postgresql-nix-monitor {})
+  gargoyle-postgresql-nix-monitor = pkgs.haskell.lib.justStaticExecutables (pkgs.haskell.lib.overrideCabal
+    (self.callCabal2nix "gargoyle-postgresql-nix-monitor" ./gargoyle-postgresql-nix-monitor {})
     (drv:{
-      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (pkgs.haskell.lib.justStaticExecutables gargoyle-postgresql-nix) ];
-    });
-  gargoyle-postgresql-connect = haskellPackages.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect {};
+      librarySystemDepends = (drv.librarySystemDepends or []) ++ [ (pkgs.haskell.lib.justStaticExecutables self.gargoyle-postgresql-nix) ];
+    }));
+  gargoyle-postgresql-connect = self.callCabal2nix "gargoyle-postgresql-connect" ./gargoyle-postgresql-connect {};
 }
