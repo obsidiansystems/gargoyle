41ac290e896f60abc68c2fa9863a7bc85827bb4b
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2020-10-20 13:16:03 -0400

    cleanup comments

--- gargoyle-postgresql-nix-monitor/src-bin/gargoyle-nix-postgres-monitor.hs
+++ gargoyle-postgresql-nix-monitor/src-bin/gargoyle-nix-postgres-monitor.hs
@@ -3,12 +3,9 @@ module Main where
 
 import Gargoyle
 import Gargoyle.PostgreSQL
--- import Gargoyle.PostgreSQL.Nix
 import System.Which
 
-
 main :: IO ()
--- main = gargoyleMain postgresNixMonitor
 main = gargoyleMain (mkPostgresGargoyleMonitor pgCtlPath shutdownPostgresFast)
   where
     pgCtlPath = $(staticWhich "pg_ctl")
