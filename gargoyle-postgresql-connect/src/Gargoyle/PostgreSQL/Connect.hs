c5afea5994e0819506ff7b6f0e754d295228a4e0
Dan Bornside <dan.bornside@gmail.com>
2022-07-18 07:26:17 -0400

    even more configurableness

--- gargoyle-postgresql-connect/src/Gargoyle/PostgreSQL/Connect.hs
+++ gargoyle-postgresql-connect/src/Gargoyle/PostgreSQL/Connect.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE DeriveGeneric #-}
 module Gargoyle.PostgreSQL.Connect where
 
@@ -7,7 +8,7 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as C8
 import Data.Pool (Pool, createPool)
 import Database.PostgreSQL.Simple (Connection, close, connectPostgreSQL)
-import Gargoyle (withGargoyle)
+import Gargoyle (withGargoyle, Gargoyle(..))
 import Gargoyle.PostgreSQL.Nix (postgresNix)
 import System.Directory (doesFileExist)
 import Data.Default.Class
@@ -18,7 +19,12 @@ import Control.Monad (guard)
 import Data.List (uncons)
 import Data.Maybe (fromMaybe)
 
--- | see $Data.Pool.createPool$
+-- | configuration options for a resource pool.
+--
+-- The exact fields of this type are subject to change; consider using
+-- the $Default$ instance when needed, and adjust as desired.
+--
+-- see $Data.Pool.createPool$ for explanation of what the fields mean.
 data PoolConfig = PoolConfig
   { _poolConfig_stripeCount :: !Int
   , _poolConfig_cacheTTL :: !NominalDiffTime
@@ -27,6 +33,50 @@ data PoolConfig = PoolConfig
 
 instance Default PoolConfig where
   def = PoolConfig 1 5 20
+
+-- | configuration options for gargoyle managed postgres instances.
+--
+-- The exact fields of this type are subject to change; consider using
+-- $mkGargoylePostgresOptions$, $gargoylePostgresOptions$,
+-- or the $Default$ instance when needed, and adjust as desired.
+data GargoylePostgresOptions = GargoylePostgresOptions
+  { _gargoylePostgresOptions_dbPath :: FilePath -- ^ path to gargoyle workDir.
+  , _gargoylePostgresOptions_gargoyleInit :: FilePath -> IO () -- ^ effect to perform on postgres database at first setup.
+  }
+
+instance Default GargoylePostgresOptions where
+  def = mkGargoylePostgresOptions "db"
+
+-- | a helper functon to get a baseline $GargoylePostgresOptions$ at a
+-- particular path.
+mkGargoylePostgresOptions :: FilePath -> GargoylePostgresOptions
+mkGargoylePostgresOptions dbPath = GargoylePostgresOptions
+  dbPath
+  (\_ -> pure ())
+
+gargoylePostgresOptions :: Maybe ByteString -> FilePath -> Either ByteString GargoylePostgresOptions
+gargoylePostgresOptions = \case
+  Just b -> \_ -> Left b
+  Nothing -> Right . mkGargoylePostgresOptions
+
+-- | look at a filepath, if it's a file, interpret its contents as a connection
+-- string, otherwise (if absent or a directory), interpret it as a path to a
+-- gargoyle managed postgres db.
+getGargoylePostgresOptions :: FilePath -> IO (Either ByteString GargoylePostgresOptions)
+getGargoylePostgresOptions dbPath = do
+  dbExists <- doesFileExist dbPath
+  if dbExists
+    -- use the file contents as the URI for an existing server
+    then Left <$> C8.readFile dbPath
+    -- otherwise assume it's a folder for a local database
+    else pure (Right (mkGargoylePostgresOptions dbPath))
+
+withDbPool
+  :: PoolConfig
+  -> Either ByteString GargoylePostgresOptions -- ^ connstring or path to gargoyle managed db
+  -> (Pool Connection -> IO a) -- ^ callback which uses the configured pool.  The configured gargoyle instance is guaranteed only until this function returns.
+  -> IO a
+withDbPool poolOpts gargoyleOpts k = withDbConnString gargoyleOpts (openDbWithConfig poolOpts >=> k)
 
 -- | Like 'withDb'' but turns the connection string into a connection
 -- 'Pool' for you and using 'error' on failure.
@@ -54,14 +104,22 @@ openDb = openDbWithConfig def
 -- database for you if it doesn't exist.
 withDb' :: FilePath -> (ByteString -> IO a) -> IO a
 withDb' dbPath f = do
-  dbExists <- doesFileExist dbPath
-  if dbExists
-    -- use the file contents as the URI for an existing server
-    then C8.readFile dbPath >>= f
-    -- otherwise assume it's a folder for a local database
-    else do
-      g <- postgresNix
-      withGargoyle g dbPath f
+  gargoyleOpts <- getGargoylePostgresOptions dbPath
+  withDbConnString gargoyleOpts f
+
+withDbConnString
+  :: Either ByteString GargoylePostgresOptions -- ^ connstring or path to gargoyle managed db
+  -> (ByteString -> IO a) -- ^ callback which uses the resulting connection string.  The configured gargoyle-postgres instance is guaranteed only until this function returns.
+  -> IO a
+withDbConnString dbOptions k = case dbOptions of
+  Left dbUri -> k dbUri
+  Right (GargoylePostgresOptions dbPath gargoyleInit) -> do
+    defaultConfig <- postgresNix
+    let myConfig = defaultConfig
+          { _gargoyle_init = _gargoyle_init defaultConfig <> gargoyleInit
+          }
+    withGargoyle myConfig dbPath k
+
 
 fmap concat $ traverse (deriveJSON (defaultOptions
   { fieldLabelModifier = \fieldLable -> fromMaybe fieldLable $ do
